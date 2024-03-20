8deb9de1374b8f28a0da7a1ad1baad8c6a0608b5
zzy001zzy <12750822@me.mcu.edu.tw>
2024-03-20 11:55:18 +0800

    week05

---
+++ week05/week05-3.cpp
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main()
+{
+    for(int i=0; i<9; i++){
+        printf("i¬O%d ", i);
+    }
+    printf("\n");
+    for(int i=1;i<=9;i++){
+        printf("i¬O%d ", i);
+    }
+    printf("\n");
+    for(int i=9;i>=0;i--){
+        printf("i¬O%d ", i);
+    }
+}
---
+++ week05/week05-3.py
@@ -0,0 +1,8 @@
+for i in range(9):
+  print('i是',i,end=' ')
+print()
+for i in range(1,9+1):
+  print('i是',i,end=' ')
+print()
+for i in range(9-1,-1,-1):
+  print('i是',i,end=' ')
